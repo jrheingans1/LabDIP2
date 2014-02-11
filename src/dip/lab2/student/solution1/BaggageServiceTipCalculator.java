b199660debc541020f24cc8d70044786f36950ce
James <James@James-PC>
2014-02-10 20:40:23 -0600

    added properties, getters/setters and constructor to BaggageServiceTipCalculator class

--- src/dip/lab2/student/solution1/BaggageServiceTipCalculator.java
+++ src/dip/lab2/student/solution1/BaggageServiceTipCalculator.java
@@ -3,13 +3,38 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package dip.lab2.student.solution1;
 
 /**
  *
  * @author James
  */
-public class BaggageServiceTipCalculator {
-    
+public class BaggageServiceTipCalculator implements TipCalculator {
+
+    private int bagCount;
+    private double baseTipPerBag;
+
+    public BaggageServiceTipCalculator(int bagCount, double baseTipPerBag) {
+        setBagCount(bagCount);
+        setBaseTipPerBag(baseTipPerBag);
+    }
+    public final double getTipAmount(){
+        return bagCount * baseTipPerBag;
+    }
+    public final int getBagCount() {
+        return bagCount;
+    }
+
+    public final void setBagCount(int bagCount) {
+        this.bagCount = bagCount;
+    }
+
+    public final double getBaseTipPerBag() {
+        return baseTipPerBag;
+    }
+
+    public final void setBaseTipPerBag(double baseTipPerBag) {
+        this.baseTipPerBag = baseTipPerBag;
+    }
+
 }
