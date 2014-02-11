1e0ddf0bce3b83286afced0143d34e34a5c2c901
James <James@James-PC>
2014-02-10 19:58:16 -0600

    Made changes to SalariedEmployee class, added getters and setters and constructor

--- src/dip/lab1/student/solution1/SalariedEmployee.java
+++ src/dip/lab1/student/solution1/SalariedEmployee.java
@@ -3,13 +3,40 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package dip.lab1.student.solution1;
 
 /**
  *
  * @author James
  */
-public class SalariedEmployee {
-    
+public class SalariedEmployee implements Employee {
+
+    private double annualSalary;
+    private double annualBonus;
+
+    public SalariedEmployee(double annualSalary, double annualBonus) {
+        setAnnualSalary(annualSalary);
+        setAnnualBonus(annualBonus);
+    }
+
+    public double getAnnualWages() {
+        return annualSalary + annualBonus;
+    }
+
+    public double getAnnualSalary() {
+        return annualSalary;
+    }
+
+    public void setAnnualSalary(double annualSalary) {
+        this.annualSalary = annualSalary;
+    }
+
+    public double getAnnualBonus() {
+        return annualBonus;
+    }
+
+    public void setAnnualBonus(double annualBonus) {
+        this.annualBonus = annualBonus;
+    }
+
 }
