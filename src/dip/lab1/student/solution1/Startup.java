a7373dbb522dd6df9045febcb8fcb469fd935946
James <James@James-PC>
2014-02-10 20:07:42 -0600

    Added test for input/output in startup and fixed an error

--- src/dip/lab1/student/solution1/Startup.java
+++ src/dip/lab1/student/solution1/Startup.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package dip.lab1.student.solution1;
 
 /**
@@ -11,8 +10,18 @@ package dip.lab1.student.solution1;
  * @author James
  */
 public class Startup {
+    
     public static void main(String[] args) {
         
+        Employee emp1 = new HourlyEmployee(15.0, 2000);
+        Employee emp2 = new SalariedEmployee(45000, 1234);
+        
+        HRService hr = new HRService();
+        
+        System.out.println("Employee 1 annual compensation: "
+                + (hr.getAnnualCompensationForEmployee(emp1)));
+        System.out.println("Employee 2 annual compensation: "
+                + (hr.getAnnualCompensationForEmployee(emp2)));
     }
-   
+    
 }
